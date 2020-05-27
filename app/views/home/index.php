f4267eee46e2cb4a6eb9edfd3da9b3e868372498
Bayu Permana Sejati <bayupermana270699@gmai.com>
2020-05-28 02:03:01 +0700

    add header, footer, raw jumbotron

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,5 +1,5 @@
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Fitur-fitur tulis disini</h1>
+    <h1 class="display-4">Fitur-fitur tulis disini yaa</h1>
   </div>
 </div>
